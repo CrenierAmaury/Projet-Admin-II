1094dcdc1a1726e526b32233d266b1ee58eefd25
CrenierAmaury <55687534+CrenierAmaury@users.noreply.github.com>
2021-02-27 15:37:56 +0100

    installation.sh + ajout links download.sh

--- DNS/download.sh
+++ DNS/download.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
+
 mkdir conf
 
-curl -o docker-compose.yml link
+curl -o docker-compose.yml https://github.com/CrenierAmaury/Projet-Admin-II/blob/main/DNS/docker-compose.yml
 
-curl -o conf/named.conf link
-curl -o conf/named.conf.default-zones link
-curl -o conf/named.conf.local link
-curl -o conf/named.conf.options link
-curl -o conf/db.l2-7.ephec-ti.be link
+curl -o conf/named.conf.default-zones https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf.default-zones
+curl -o conf/named.conf.local https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf.local
+curl -o conf/named.conf.options https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf.options
+curl -o conf/db.l2-7.ephec-ti.be https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/db.l2-7.ephec-ti.be
---
+++ WEB/installation.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker pull monimage
+
+mkdir web
+cd web
+curl -o download.sh https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/download.sh
+chmod +x download.sh
+
+./download.sh
+
+docker-compose up
