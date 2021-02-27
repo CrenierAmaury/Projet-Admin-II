da51f3b13b088d071a17f32b4d68ccc4062b34e1
CrenierAmaury <55687534+CrenierAmaury@users.noreply.github.com>
2021-02-27 14:11:34 +0100

    mise en place arborescence et docker dns

---
+++ DNS/download.sh
@@ -0,0 +1,9 @@
+mkdir conf
+
+curl -o docker-compose.yml link
+
+curl -o conf/named.conf link
+curl -o conf/named.conf.default-zones link
+curl -o conf/named.conf.local link
+curl -o conf/named.conf.options link
+curl -o conf/db.l2-7.ephec-ti.be link
