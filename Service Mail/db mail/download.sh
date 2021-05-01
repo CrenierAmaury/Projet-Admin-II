ae723d08cb39b00107d73f4e7fac943d96819cd0
CastermaneRobin <55954496+CastermaneRobin@users.noreply.github.com>
2021-05-01 09:37:10 +0200

    Update download.sh

--- Service Mail/db mail/download.sh
+++ Service Mail/db mail/download.sh
@@ -1,3 +1,3 @@
 curl -o docker-compose.yml https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/Service%20Mail/db%20mail/docker-compose.yml
 mkdir sql-scripts
-curl -o sql-scripts/db-init.sql https://raw.githubusercontent.com/ReseauEntrepriseGroupeB/Mail/main/bdd/sql-scripts/db-init.sql
+curl -o sql-scripts/db-init.sql https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/Service%20Mail/db%20mail/sql-scripts/db-init.sql.sql
