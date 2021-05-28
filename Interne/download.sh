fc80fe501dbb3295b336cf026bed57e81dcf7177
CastermaneRobin <robin@castermane.be>
2021-05-28 21:37:34 +0200

    ajout des postes utilisateurs pour le réseau interne

---
+++ Interne/download.sh
@@ -0,0 +1,2 @@
+curl -o docker-compose.yml https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/Interne/docker-compose.yml
+curl -o resolv.conf https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/Interne/resolv.conf
