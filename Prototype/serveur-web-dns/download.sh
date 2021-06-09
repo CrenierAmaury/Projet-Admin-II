a60753f8d379fd328613e2b40a6933c12e71c713
CrenierAmaury <55687534+CrenierAmaury@users.noreply.github.com>
2021-06-09 06:27:15 +0200

    fix compose web-dns download link

--- Prototype/serveur-web-dns/download.sh
+++ Prototype/serveur-web-dns/download.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+curl -o docker-compose.yml https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/Prototype/serveur-web-dns/docker-compose.yml
+
 cd dns
 mkdir conf
-
-curl -o docker-compose.yml https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/docker-compose.yml
 
 curl -o conf/named.conf https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf
 curl -o conf/named.conf.local https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf.local
@@ -13,6 +13,6 @@ curl -o conf/db.l2-7.ephec-ti.be https://raw.githubusercontent.com/CrenierAmaury
 cd ../web
 mkdir sites
 
-curl -o docker-compose.yml https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/docker-compose.yml
-
 curl -o sites/index.html https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/woody-toys-vitrine/index.html
+
+cd ..
