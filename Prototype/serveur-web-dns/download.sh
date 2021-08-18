4c486dbe558351606f03d2a4c319d376ffbaa6dd
CrenierAmaury <amaurycrenier@gmail.com>
2021-08-18 11:49:33 +0200

    fix dnssec

--- Prototype/serveur-web-dns/download.sh
+++ Prototype/serveur-web-dns/download.sh
@@ -9,6 +9,7 @@ curl -o conf/named.conf https://raw.githubusercontent.com/CrenierAmaury/Projet-A
 curl -o conf/named.conf.local https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf.local
 curl -o conf/named.conf.options https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/named.conf.options
 curl -o conf/db.l2-7.ephec-ti.be https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/db.l2-7.ephec-ti.be
+curl -o conf/dnssec_key.sh https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/DNS/conf/dnssec_key.sh
 
 cd ../web
 mkdir sites
