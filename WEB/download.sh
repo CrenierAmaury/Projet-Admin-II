f2ba86f8b20aac3a76fad782c5a047edfc288a7e
CrenierAmaury <amaurycrenier@gmail.com>
2021-08-18 18:07:25 +0200

    passage en copy dockerfile web

--- WEB/download.sh
+++ WEB/download.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-mkdir sites
-mkdir sites/www
-mkdir sites/b2b
-
 curl -o docker-compose.yml https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/docker-compose.yml
 
-curl -o sites/www/index.html https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/woody-toys-vitrine/index.html
-curl -o sites/b2b/index.php https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/woody-toys-b2b/index.php
+curl -o index.html https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/woody-toys-vitrine/index.html
+curl -o index.php https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/woody-toys-b2b/index.php
 
-curl -o sites/nginx.conf https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/nginx.conf
+curl -o nginx.conf https://raw.githubusercontent.com/CrenierAmaury/Projet-Admin-II/main/WEB/nginx.conf
